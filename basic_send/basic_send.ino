f789358af808ead86b86b4a33f8128a276a4541d
Tianyang Lu <til159@ucsd.edu>
2019-06-07 02:44:57 -0700

    receive boardcast msg from listener

--- basic_send/basic_send.ino
+++ basic_send/basic_send.ino
@@ -8,7 +8,7 @@
 //************************************************************
 #include "painlessMesh.h"
 
-#define   MESH_PREFIX     "name"
+#define   MESH_PREFIX     "MeshNetworkWIFI"
 #define   MESH_PASSWORD   "qwertyuiop"
 #define   MESH_PORT       5555
 
@@ -19,17 +19,32 @@ painlessMesh  mesh;
 void sendMessage() ; // Prototype so PlatformIO doesn't complain
 
 Task taskSendMessage( TASK_SECOND * 1 , TASK_FOREVER, &sendMessage );
+int toRead = 0;
 
 void sendMessage() {
-  String msg = "Hello from node ";
-  msg += mesh.getNodeId();
+  String msg;
+  if (toRead == 1){
+    msg = "hallRead value: ";
+    int val = digitalRead(1);
+    msg += val;
+    msg += "from node: ";
+    msg += mesh.getNodeId();
+      
+  } else {
+    msg = "Hello from node: ";
+    msg += mesh.getNodeId();
+  }
   mesh.sendBroadcast( msg );
   taskSendMessage.setInterval( random( TASK_SECOND * 1, TASK_SECOND * 5 ));
+  Serial.printf("Node send");
 }
 
 // Needed for painless library
 void receivedCallback( uint32_t from, String &msg ) {
-  Serial.printf("startHere: Received from %u msg=%s\n", from, msg.c_str());
+  if (msg.indexOf("read") >= 0){
+    toRead = 1;
+  } 
+  Serial.printf("Node: Received from %u msg=%s%d\n", from, msg.c_str(), toRead);
 }
 
 void newConnectionCallback(uint32_t nodeId) {
@@ -46,8 +61,9 @@ void nodeTimeAdjustedCallback(int32_t offset) {
 
 void setup() {
   Serial.begin(115200);
+  pinMode(0, INPUT);
 
-//mesh.setDebugMsgTypes( ERROR | MESH_STATUS | CONNECTION | SYNC | COMMUNICATION | GENERAL | MSG_TYPES | REMOTE ); // all types on
+  //mesh.setDebugMsgTypes( ERROR | MESH_STATUS | CONNECTION | SYNC | COMMUNICATION | GENERAL | MSG_TYPES | REMOTE ); // all types on
   mesh.setDebugMsgTypes( ERROR | STARTUP  );  // set before init() so that you can see startup messages
 
   mesh.init( MESH_PREFIX, MESH_PASSWORD, &userScheduler, MESH_PORT, WIFI_AP_STA );
@@ -58,6 +74,7 @@ void setup() {
 
   userScheduler.addTask( taskSendMessage );
   taskSendMessage.enable();
+  Serial.printf("Node setUp");
 }
 
 void loop() {
